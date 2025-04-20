3f4b14315f5558789c8344ce728ece38074a055e
robot <robot@recompile.me>
2025-04-20 20:59:16 +0200

    Learnt how to handle files using syscalls

--- main.c
+++ main.c
@@ -1,39 +1,53 @@
-// Linux x86_64
-#ifdef __x86_64__
-asm(
-		".global _start\n"
-		"_start:\n"
-		"   xorl %ebp,%ebp\n"       // mark outermost stack frame
-		"   movq 0(%rsp),%rdi\n"    // get argc
-		"   lea 8(%rsp),%rsi\n"     // the arguments are pushed just below, so argv = %rbp + 8
-		"   call onelang_main\n"       // call our onelang_main
-		"   movq %rax,%rdi\n"       // take the main return code and use it as first argument for...
-		"   movl $60,%eax\n"        // ... the exit syscall
-		"   syscall\n"
-		"   int3\n");               // just in case
+// main.c
+// Compile with: gcc -nostdlib -static -o main main.c
 
-asm(
-		"onelang_write:\n"             // write syscall wrapper; the calling convention is pretty much ok as is
-		"   movq $1,%rax\n"         // 1 = write syscall on x86_64
-		"   syscall\n"
-		"   ret\n");
-#endif
+void _start() {
+    const char filename[] = "output.txt";
+    const char msg[] = "Written from C!\n";
+    long fd;
 
-int onelang_write(int fd, const void *buf, unsigned count);
+    // open("output.txt", O_WRONLY|O_CREAT|O_TRUNC, 0644)
+    asm volatile (
+        "mov $2, %%rax\n"          // syscall: open
+        "mov %1, %%rdi\n"          // filename
+        "mov $0x241, %%rsi\n"      // flags: O_WRONLY|O_CREAT|O_TRUNC
+        "mov $420, %%rdx\n"        // mode: rw-r--r-- (0644 octal = 420 decimal)
+        "syscall\n"
+        "mov %%rax, %0\n"          // store fd
+        : "=r"(fd)
+        : "r"(filename)
+        : "%rax", "%rdi", "%rsi", "%rdx"
+    );
 
-unsigned my_strlen(const char *ch) {
-  const char *ptr;
-  for(ptr = ch; *ptr; ++ptr);
-  return ptr-ch;
-}
+    // write(fd, msg, 16)
+    asm volatile (
+        "mov $1, %%rax\n"          // syscall: write
+        "mov %0, %%rdi\n"          // fd
+        "mov %1, %%rsi\n"          // buf
+        "mov $16, %%rdx\n"         // count
+        "syscall\n"
+        :
+        : "r"(fd), "r"(msg)
+        : "%rax", "%rdi", "%rsi", "%rdx"
+    );
 
-int onelang_main(int argc, char *argv[]) {
+    // close(fd)
+    asm volatile (
+        "mov $3, %%rax\n"          // syscall: close
+        "mov %0, %%rdi\n"          // fd
+        "syscall\n"
+        :
+        : "r"(fd)
+        : "%rax", "%rdi"
+    );
 
-  for(int i = 0; i < argc; ++i) {
-		int len = my_strlen(argv[i]);
-		onelang_write(1, argv[i], len);
-		onelang_write(1, "\n", 1);
-	}
-
-  return 0;
+    // exit(0)
+    asm volatile (
+        "mov $60, %%rax\n"
+        "xor %%rdi, %%rdi\n"
+        "syscall\n"
+        :
+        :
+        : "%rax", "%rdi"
+    );
 }
