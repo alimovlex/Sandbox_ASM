2c81e5ccbb4242923207f2e05d038a711626e48a
robot <robot@recompile.me>
2025-04-21 02:56:04 +0200

    Called some functions from the syscalls library. Works fine.

--- asm/sandbox.asm
+++ asm/sandbox.asm
@@ -1,11 +1,27 @@
 %include "utils.asm"
 section .data
-    msg db 'Hello, World!', 0xA
+    msg db 'Welcome to my assembly x64 Sandbox!', 0xA
+    prompt db 'Please enter any message below and it will be echoed here...', 0xA
     len equ $ - msg
+    len2 equ $ - prompt
+    name db "sandbox.log",0
+    name2 db "sandbox_renamed.log",0
+
+section .bss
+    buffer resb 1
+    fd resw 1
+    time resb 1
 
 section .text
     global _start
 
 _start:
-    PAUSE
+    WRITE STDOUT,msg, len
+    WRITE STDOUT,prompt, len2
+    READ STDIN,buffer, 128
+    WRITE STDOUT,buffer, 128
     EXIT SUCCESS_EXIT
+
+;CREAT name,O_CREAT
+;TIME time
+;RENAME name,name2
